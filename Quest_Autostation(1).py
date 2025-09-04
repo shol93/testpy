8dfb20d025299d64545eb8658739dd39a492d833
Oleg <sh.ol.1993@outlook.com>
2025-09-05 01:51:29 +0300

    Autostation(3)

--- Quest_Autostation(1).py
+++ Quest_Autostation(1).py
@@ -4,19 +4,20 @@ num_tickets = int(input("Количество проданных билетов:
 bus_capacity=20 # Кол-во мест в автобусе
 
 bus_quantity=num_tickets//bus_capacity # Полный автобус
-num_tickets=num_tickets%bus_capacity # Ост. пассажиры
+num_tickets_left=num_tickets%bus_capacity # Ост. пассажиры
 
 has_partial_bus=False
 empty_seats=0
 
-if num_tickets>=bus_capacity/2:
-    bus_quantity+=1
-    has_partial_bus=True
-    empty_seats=bus_capacity-num_tickets
-    num_tickets=0
+if num_tickets_left:
+    if num_tickets_left>=bus_capacity/2:
+        bus_quantity+=1
+        has_partial_bus=True
+        empty_seats=bus_capacity-num_tickets_left
+        num_tickets_left=0
 
     
 
 
 
-print(bus_quantity, num_tickets, has_partial_bus, empty_seats)
+print(bus_quantity, num_tickets_left, has_partial_bus, empty_seats)
